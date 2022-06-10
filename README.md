aec39c4b30213d1a396944fe807e23e612c418d9
sinashahoveisi <shahoveisysina@yahoo.com>
2022-06-11 02:08:32 +0430

    config: initial config project

---
+++ README.md
@@ -0,0 +1,30 @@
+# DrillD
+
+> The best DrillDown package for react
+
+[![NPM](https://img.shields.io/npm/v/drilld.svg)](https://www.npmjs.com/package/drilld) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save drilld
+```
+
+## Usage
+
+```tsx
+import React, { Component } from 'react'
+
+import MyComponent from 'drilld'
+import 'drilld/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+© MIT [sinashahoveisi](https://github.com/sinashahoveisi)
---
+++ projects/example/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Hello} from 'react-drilld';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hello name="sina" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
